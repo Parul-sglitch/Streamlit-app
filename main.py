f5b10e7dc979faf7f48053fb6767a1be334cd53c
Parul Singh <78781760+Parul-sglitch@users.noreply.github.com>
2021-05-28 23:31:16 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,24 @@
+import streamlit as st
+import pandas as pd
+from PIL import Image
+from apps import pp1,pp2,pp3,pp4,pp5
+def main():
+    col1,col2=st.beta_columns([1,2])
+    with col1:
+        img = Image.open("C:/Users/PARUL KUMARI/Desktop/project/rig.jpeg")
+        st.image(img, width=200)    
+    with col2:
+        st.title("Original Oil In Place & Recovery Factor Calculation")
+    st.sidebar.title('Different Drive Reservoirs')
+    status=st.sidebar.radio('Select type of reservoir:',('Solution gas drive reservoir (above bubble point pressure)','Solution gas drive reservoir (below bubble point pressure)','Gas cap drive reservoir','Water drive reservoir','Combination drive reservoir'))
+    if status=='Solution gas drive reservoir (above bubble point pressure)':
+        pp1.app()
+    elif status=='Solution gas drive reservoir (below bubble point pressure)':
+        pp2.app()
+    elif status=='Gas cap drive reservoir':
+        pp3.app()
+    elif status=='Water drive reservoir':
+        pp4.app()
+    elif status=='Combination drive reservoir':
+        pp5.app()
+main()
